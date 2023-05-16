6a21b7e4a346c5b976df68a7b90d6bfa88614fc2
Belzeus30 <radekraisigl@gmail.com>
2023-05-16 22:12:06 +0200

    ano

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AkceData;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('api/events', [AkceData::class, 'GetAkceData']);
+Route::get('/events', [AkceData::class, 'GetAkceData']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
