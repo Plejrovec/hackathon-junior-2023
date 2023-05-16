2cdfa2925e5b36eceff5d56f88738612fa6a7bf0
Plejrovec <121168824+Plejrovec@users.noreply.github.com>
2023-05-16 18:24:17 +0200

    Delete src directory

--- src/routes/KulturniAkce/Kurzy.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Kurzy() {
-  return (
-    <div>Kurzy</div>
-  )
-}
--- src/routes/TuristickeAkce/PesiVylety.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function PesiVylety() {
-  return (
-    <div>PesiVylety</div>
-  )
-}
