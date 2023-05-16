12d3f477bdaa17dff35f2318902392a84be2a4cd
Belzeus30 <radekraisigl@gmail.com>
2023-05-16 19:28:04 +0200

    basic css

--- react/src/routes/default.jsx
+++ react/src/routes/default.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Default() {
   return (
-    <div>
-      <h1 className="mainname">Co se děje v Brně? Všechny akce pohodlně, levněji na jednom místě.</h1>
-    </div>
+    <div>default</div>
   )
 }
