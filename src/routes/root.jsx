892860a53e1c456565df9ec399937957452694e0
Belzeus30 <radekraisigl@gmail.com>
2023-05-16 17:40:11 +0200

    locks unicodes

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -56,6 +56,17 @@ export default function Root() {
                   <a href={`vysokohorskaTuristika`}>Vysokohorska turistika</a>
                 </li>
               </ul>
+
+              <p>Obecné</p>
+              <ul>
+                <li>
+                <a href={``}><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 448 512" className="lock"><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" /></svg> Můj účet</a>
+                </li>
+                <li>
+                  <a href={``}><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 448 512" className="lock"><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" /></svg> Moje akce</a>
+                </li>
+              </ul>
+              
               <a className="back-arrow" href={"/"}>&#129044;</a>
           </aside>
           <main id="detail" class="content">
