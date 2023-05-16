6be73eb8166d1bdb7b26375c31caa388e4b2eb42
Belzeus30 <radekraisigl@gmail.com>
2023-05-16 23:57:00 +0200

    created event list

--- react/src/components/Event.jsx
+++ react/src/components/Event.jsx
@@ -1,50 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Event = () => {
-  const [eventData, setEventData] = useState({
-    ID: '',
-    name: '',
-    text: '',
-    tickets: '',
-    tickets_info: '',
-    images: '',
-    url: '',
-    categories: '',
-    parent_festivals: '',
-    organizer_email: '',
-    tickets_url: '',
-    name_en: '',
-    text_en: '',
-    url_en: '',
-    tickets_url_en: '',
-    latitude: '',
-    longitude: '',
-    date_from: '',
-    date_to: '',
-    first_image: '',
-    ObjectId: '',
-    GlobalID: '',
-  });
+    const [eventData, setEventData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:8000/api/events"
+                );
+                const data = response.data.flat(Infinity).slice(0, 3);
+                setEventData(data);
+                console.log(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const fetchLocation = async (latitude, longitude) => {
       try {
-        const response = await axios.get('api/events'); // Replace '/api/data' with your Laravel API endpoint
-        setEventData(response.data);
+        const apiKey = 'AIzaSyAK7PSYID6RF8xxyKp6n4PX80vuqArD_QA'; // Replace with your Google Maps Geocoding API key
+  
+        const response = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`
+        );
+  
+        const results = response.data.results;
+        if (results.length > 0) {
+          const address = results[0].formatted_address;
+          console.log(address);
+        }
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchData();
-  }, []);
+    return (
+        <>
+            {/* Render your component using the fetched eventData */}
+            {eventData.map((event) => {
+                const image = event.images.split(",")[0];
 
-  return (
-    <>
-      {/* Render your component using the fetched eventData */}
-    </>
-  );
+                return (
+                    <div key={event.ID}>
+                        <img src={image} alt="" />
+                        <div className="right">
+                            <div className="top">
+                                <p
+                                    dangerouslySetInnerHTML={{
+                                        __html: event.name,
+                                    }}
+                                ></p>
+                                <button>Vstupenky</button>
+                            </div>
+                            <div className="bottom">
+                                <div className="date">{event.date_from}</div>
+                                <div className="location">{fetchLocation(event.latitude,event.longitude)}</div>
+                            </div>
+                        </div>
+
+                                    
+                    </div>
+                );
+            })}
+        </>
+    );
 };
 
 export default Event;
