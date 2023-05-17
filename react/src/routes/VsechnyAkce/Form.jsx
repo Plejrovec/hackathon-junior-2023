757c8ff080cf081b5bba65375d1223013f8171f6
Belzeus30 <radekraisigl@gmail.com>
2023-05-17 10:09:44 +0200

    ajkhcjlkaejlifajhafeljhaflha

--- react/src/routes/VsechnyAkce/Form.jsx
+++ react/src/routes/VsechnyAkce/Form.jsx
@@ -6,12 +6,12 @@ export default function Form() {
         <h2>Akce</h2>
     <div className='form'>
         
-        <input placeholder="text"></input>
-        <input placeholder="Kategorie"></input>
-        <input placeholder="Obrazek"></input>
-        <input placeholder="Datum od"></input>
-        <input placeholder="Datum do"></input>
-        <input placeholder="Misto"></input>
+        <input className='inpt' placeholder="text"></input>
+        <input className='inpt' placeholder="Kategorie"></input>
+        <input className='inpt' placeholder="Obrazek"></input>
+        <input className='inpt' placeholder="Datum od"></input>
+        <input className='inpt' placeholder="Datum do"></input>
+        <input className='inpt' placeholder="Misto"></input>
         <a  className="sbt" href="/">Ulozit</a>
 
     </div>
