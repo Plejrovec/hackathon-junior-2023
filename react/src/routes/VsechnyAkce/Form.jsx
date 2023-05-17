96ddadca53ae525bd77452f8e9a03e6c5b38abd3
Belzeus30 <radekraisigl@gmail.com>
2023-05-17 09:22:23 +0200

    kfdsajfklsadk awega

---
+++ react/src/routes/VsechnyAkce/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Form() {
+  return (
+    <div>
+        <input value="text">Text</input>
+        <input>Kategorie(vyber)</input>
+        <input>Obrazek</input>
+        <input>Datum od</input>
+        <input>Datum do</input>
+        <input>Misto</input>
+
+    </div>
+  )
+}
