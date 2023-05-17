536114fd9fd84c7be5557578c67de00d0ed16195
dejwiikkxd <dejwkrevnak@outlook.com>
2023-05-17 10:05:36 +0200

    lmao

--- react/src/routes/root.jsx
+++ react/src/routes/root.jsx
@@ -5,7 +5,7 @@ import foot from "../imgs/footer.png"
 export default function Root() {
     return (
       <div className="main">
-        <img src={nav} ></img>
+        <a className="HEAD" href="https://www.brnoid.cz/cs/"><img src={nav}></img></a>
         <section className="main-content">
           <aside  className="sidebar">
             
@@ -53,10 +53,10 @@ export default function Root() {
               <p>Obecné</p>
               <ul>
                 <li>
-                <a href={``}><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 448 512" className="lock"><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" /></svg> Můj účet</a>
+                <a href={`https://www.brnoid.cz/cs/overeni`}><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 448 512" className="lock"><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" /></svg> Můj účet</a>
                 </li>
                 <li>
-                  <a href={``}><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 448 512" className="lock"><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" /></svg> Moje akce</a>
+                  <a href={`https://www.brnoid.cz/cs/`}><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 448 512" className="lock"><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" /></svg> Moje akce</a>
                 </li>
               </ul>
               
