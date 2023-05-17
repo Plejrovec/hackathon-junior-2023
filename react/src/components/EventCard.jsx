1a62a74192dba1c46eb37e5296eb9918d24a5bcb
Belzeus30 <radekraisigl@gmail.com>
2023-05-17 09:45:12 +0200

    fjakjgfajgfaoig

--- react/src/components/EventCard.jsx
+++ react/src/components/EventCard.jsx
@@ -15,7 +15,7 @@ export default function EventCard(props) {
               
               <div className="bottom">
                 <div className="date"><img className='cal' src={cal}></img> {props.event.date_from + "" + props.event.date_to}</div>
-                <div className="location">{" "}</div>
+                <div className="location"><a href={props.event.Address}></a></div>
                 
               </div>
             </div>
